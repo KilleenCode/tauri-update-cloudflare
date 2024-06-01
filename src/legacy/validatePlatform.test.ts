06847749d3fbb1e4a2b88cd0ca9925d0be27aa26
Mackenly Jones <53151058+mackenly@users.noreply.github.com>
2024-06-01 12:27:32 -0400

    Prettier

--- src/legacy/validatePlatform.test.ts
+++ src/legacy/validatePlatform.test.ts
@@ -4,17 +4,33 @@ import { describe, it, expect } from 'vitest';
 
 describe('validatePlatform', () => {
     it('should return the platform if it is valid using enum', () => {
-        expect(validatePlatform(LEGACY_AVAILABLE_PLATFORMS.MacOS)).toBe(LEGACY_AVAILABLE_PLATFORMS.MacOS);
-        expect(validatePlatform(LEGACY_AVAILABLE_PLATFORMS.Win32)).toBe(LEGACY_AVAILABLE_PLATFORMS.Win32);
-        expect(validatePlatform(LEGACY_AVAILABLE_PLATFORMS.Win64)).toBe(LEGACY_AVAILABLE_PLATFORMS.Win64);
-        expect(validatePlatform(LEGACY_AVAILABLE_PLATFORMS.Linux)).toBe(LEGACY_AVAILABLE_PLATFORMS.Linux);
+        expect(validatePlatform(LEGACY_AVAILABLE_PLATFORMS.MacOS)).toBe(
+            LEGACY_AVAILABLE_PLATFORMS.MacOS
+        );
+        expect(validatePlatform(LEGACY_AVAILABLE_PLATFORMS.Win32)).toBe(
+            LEGACY_AVAILABLE_PLATFORMS.Win32
+        );
+        expect(validatePlatform(LEGACY_AVAILABLE_PLATFORMS.Win64)).toBe(
+            LEGACY_AVAILABLE_PLATFORMS.Win64
+        );
+        expect(validatePlatform(LEGACY_AVAILABLE_PLATFORMS.Linux)).toBe(
+            LEGACY_AVAILABLE_PLATFORMS.Linux
+        );
     });
 
     it('should return the platform if it is valid using string', () => {
-        expect(validatePlatform('darwin')).toBe(LEGACY_AVAILABLE_PLATFORMS.MacOS);
-        expect(validatePlatform('win32')).toBe(LEGACY_AVAILABLE_PLATFORMS.Win32);
-        expect(validatePlatform('win64')).toBe(LEGACY_AVAILABLE_PLATFORMS.Win64);
-        expect(validatePlatform('linux')).toBe(LEGACY_AVAILABLE_PLATFORMS.Linux);
+        expect(validatePlatform('darwin')).toBe(
+            LEGACY_AVAILABLE_PLATFORMS.MacOS
+        );
+        expect(validatePlatform('win32')).toBe(
+            LEGACY_AVAILABLE_PLATFORMS.Win32
+        );
+        expect(validatePlatform('win64')).toBe(
+            LEGACY_AVAILABLE_PLATFORMS.Win64
+        );
+        expect(validatePlatform('linux')).toBe(
+            LEGACY_AVAILABLE_PLATFORMS.Linux
+        );
     });
 
     it('should return undefined if the platform is not valid', () => {
